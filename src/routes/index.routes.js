cbecabe83b8885e35aa0325ef2bd8276677dd55e
Julianeves01 <julianvs2007@gmail.com>
2024-10-17 14:25:19 -0300

    Create servidor

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,13 +1,13 @@
 import { Router } from "express";
 
 // Lista de importação das rotas do projeto
-import suspeitosRoutes from "./suspeitos.routes.js";
+import suspeitosRoutes from "../routes/suspeitos.routes.js";
 
 const routes = Router();
 
 // Rota raiz para teste
 routes.get("/", (req, res) => {
-  return res.status(200).json({ message: "Servidor ta funfando amores!" });
+  return res.status(201).json({ message: "Servidor ta funfando amores!" });
 });
 
 // Lista de uso das rotas do projeto
--- src/routes/suspeitos.routes.js
+++ src/routes/suspeitos.routes.js
@@ -1,47 +1,52 @@
 import { Router } from "express";
 
-
 const suspeitosRoutes = Router();
 let suspeitos = [
-    {
-            id: Math.floor(Math.random() * 99 + 1),
-            nome: "Jollie Santos",
-            idade: 29,
-            descricaoFisica: "Altura média, cabelo loiro, tatuagem delicada nos braços, olhos verdes",
-            envolvimento: sim,
-    },
-    {
-            id: Math.floor(Math.random() * 99 + 1),
-            nome: "Jane Smith",
-            idade: 30,
-            descricaoFisica: "Baixa, cabelo moreno, olho castanho claro",
-            envolvimento: não,
-    },
-    {
-        id: Math.floor(Math.random() * 99 + 1),
-            nome: "Jay z",
-            idade: 54,
-            descricaoFisica: "Alto, cabelo preto, olhos pretos",
-            envolvimento: sim,
-    },
+  {
+    id: Math.floor(Math.random() * 99 + 1),
+    nome: "Jollie Santos",
+    idade: 28,
+    descricaoFisica: "Altura média, cabelo loiro, tatuagem delicada nos braços, olhos verdes",
+    envolvimento: true,
+  },
+  {
+    id: Math.floor(Math.random() * 99 + 1),
+    nome: "Jane Smith",
+    idade: 30,
+    descricaoFisica: "Baixa, cabelo moreno, olho castanho claro",
+    envolvimento: false,
+  },
+  {
+    id: Math.floor(Math.random() * 99 + 1),
+    nome: "Jay Z",
+    idade: 54,
+    descricaoFisica: "Alto, cabelo preto, olhos pretos",
+    envolvimento: true,
+  },
 ];
 
 // Rota para listar todos os suspeitos
 suspeitosRoutes.get("/", (req, res) => {
-    return res.status(200).json(suspeitos);
-  });
-  
-//Rota para criar um novo suspeito.
-const novoSuspeito = {
+  return res.status(200).json(suspeitos);
+});
+
+// Rota para criar um novo suspeito
+suspeitosRoutes.post("/", (req, res) => {
+  const { nome, idade, descricaoFisica, envolvimento } = req.body;
+
+  const novoSuspeito = {
     id: Math.floor(Math.random() * 99 + 1),
     nome,
     idade,
+    descricaoFisica,
     envolvimento,
-  }
-  suspeitos.push(novoSuspeito)
+  };
+
+  suspeitos.push(novoSuspeito);
   return res.status(201).json({
-    message: "suspeito cadastrado com sucesso!",
+    message: "Suspeito cadastrado com sucesso!",
     novoSuspeito,
-  })
+  });
+});
 
 export default suspeitosRoutes;
