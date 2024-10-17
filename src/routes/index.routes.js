b047eeb188a93d4565861f24c5a936e958ac7825
Julianeves01 <julianvs2007@gmail.com>
2024-10-17 14:31:31 -0300

    Create servidor

--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -2,14 +2,12 @@ import { Router } from "express";
 
 // Lista de importação das rotas do projeto
 import suspeitoRoutes from "../routes/suspeitos.routes.js";
-import suspeitosRoutes from "../routes/suspeitos.routes.js";
 
 const routes = Router();
 
 // Rota raiz para teste
 routes.get("/", (req, res) => {
   return res.status(200).json({ message: "servidor esta funfando amoress!" });
-  return res.status(201).json({ message: "Servidor ta funfando amores!" });
 });
 
 // Lista de uso das rotas do projeto
